bff428c280df0ee527b7e51cf3a27b801bc10774
Abdulkadir <musaabdulkadir90@gmail.com>
2026-08-11 17:05:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
+        Add to Cart
+      </button>
+    </>
+  )
+}
+
+export default App
