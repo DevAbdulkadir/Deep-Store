b6081f7ea484349c7ad1c0c6036f7a16aba6b1b5
DevAbdulkadir <Abduldlegend@gmail.com>
2026-08-12 00:54:10 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import { products } from '../data/products'
 
 function App() {
 
+  const shoeProducts = products.filter(p => p.category === "fashion");
+  const handleClick = () => {
+    console.log(shoeProducts);
+  }
+  
+
   return (
     <>
-      <button className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
+      <button className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700" onClick={handleClick}>
         Add to Cart
       </button>
     </>
